8e59cacc0c4b51cb00c8c36363f3e92b104a409d
kimhyungjun <hyungjoon6876@gmail.com>
2018-05-30 23:00:30 +0900

    init

---
+++ src/main/java/com/example/microservicepurchase/MicroServicePurchaseApplication.java
@@ -0,0 +1,12 @@
+package com.example.microservicepurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicePurchaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServicePurchaseApplication.class, args);
+    }
+}
